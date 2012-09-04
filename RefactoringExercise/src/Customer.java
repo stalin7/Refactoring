452c7ec7eace78f04306a56fa0ff1b866a487e34
stalin7 <stalindesa7@gmail.com>
2012-09-04 11:22:55 +0530

    Moved method amountFor() to Rental class and renamed it to getCharge()

--- RefactoringExercise/src/Customer.java
+++ RefactoringExercise/src/Customer.java
@@ -24,7 +24,7 @@ class Customer {
 	            Rental each = (Rental) rentals.nextElement();
 
 	            //determine amounts for each line
-	            thisAmount = amountFor(thisAmount, each);
+	            thisAmount = each.getCharge(thisAmount);
 
 	            // add frequent renter points
 	            frequentRenterPoints ++;
@@ -45,25 +45,5 @@ class Customer {
 	        return result;
 
 	    }
-
-	public double amountFor(double thisAmount, Rental each) {
-		switch (each.getMovie().getPriceCode()) {
-		    case Movie.REGULAR:
-		        thisAmount += 2;
-		        if (each.getDaysRented() > 2)
-		            thisAmount += (each.getDaysRented() - 2) * 1.5;
-		        break;
-		    case Movie.NEW_RELEASE:
-		        thisAmount += each.getDaysRented() * 3;
-		        break;
-		    case Movie.CHILDRENS:
-		        thisAmount += 1.5;
-		        if (each.getDaysRented() > 3)
-		            thisAmount += (each.getDaysRented() - 3) * 1.5;
-		        break;
-
-		}
-		return thisAmount;
-	}
 }
 
