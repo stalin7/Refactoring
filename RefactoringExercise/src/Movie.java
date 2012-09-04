c3668018ef4c9d32f13de8165bf87262e5ff2a59
stalin7 <stalindesa7@gmail.com>
2012-09-04 12:16:31 +0530

    Move method getFrequentRenterPoints() from Rental class to Movie class and updated method calls appropriately

--- RefactoringExercise/src/Movie.java
+++ RefactoringExercise/src/Movie.java
@@ -47,6 +47,16 @@ public class Movie {
 	
 		}
 		return thisAmount;
+	}
+
+
+	public int getFrequentRenterPoints(int daysRented) {
+		// add frequent renter points
+		
+		// add bonus for a two day new release rental
+		if ((getPriceCode() == Movie.NEW_RELEASE) && daysRented > 1) return 2;
+		else
+			return 1;
 	};
 	 }
 
