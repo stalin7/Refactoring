c343e3e52bbf1861590a05d54d38d081585d01a2
stalin7 <stalindesa7@gmail.com>
2012-09-04 11:40:04 +0530

    Extract method getFrequentRenterPoints()

--- RefactoringExercise/src/Customer.java
+++ RefactoringExercise/src/Customer.java
@@ -25,11 +25,8 @@ class Customer {
 
 	           
 
-	            // add frequent renter points
-	            frequentRenterPoints ++;
-	            // add bonus for a two day new release rental
-	            if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) &&
-	each.getDaysRented() > 1) frequentRenterPoints ++;
+	            frequentRenterPoints = getFrequentRenterPoints(
+						frequentRenterPoints, each);
 
 	            //show figures for this rental
 	            result += "\t" + each.getMovie().getTitle()+ "\t" +
@@ -44,6 +41,15 @@ class Customer {
 	        return result;
 
 	    }
+
+	public int getFrequentRenterPoints(int frequentRenterPoints, Rental each) {
+		// add frequent renter points
+		frequentRenterPoints ++;
+		// add bonus for a two day new release rental
+		if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) &&
+each.getDaysRented() > 1) frequentRenterPoints ++;
+		return frequentRenterPoints;
+	}
 	   
 	 
 }
