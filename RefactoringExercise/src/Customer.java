c805f475c821f55ed81eda14e7801ad8c65cdaae
stalin7 <stalindesa7@gmail.com>
2012-09-04 11:37:42 +0530

    Replaced temp variable thisAmount with query each.getCharge()

--- RefactoringExercise/src/Customer.java
+++ RefactoringExercise/src/Customer.java
@@ -20,11 +20,10 @@ class Customer {
 	        Enumeration rentals = _rentals.elements();
 	        String result = "Rental Record for " + getName() + "\n";
 	        while (rentals.hasMoreElements()) {
-	            double thisAmount = 0;
+	            
 	            Rental each = (Rental) rentals.nextElement();
 
-	            //determine amounts for each line
-	            thisAmount = each.getCharge();
+	           
 
 	            // add frequent renter points
 	            frequentRenterPoints ++;
@@ -34,8 +33,8 @@ class Customer {
 
 	            //show figures for this rental
 	            result += "\t" + each.getMovie().getTitle()+ "\t" +
-	String.valueOf(thisAmount) + "\n";
-	            totalAmount += thisAmount;
+	String.valueOf( each.getCharge()) + "\n";
+	            totalAmount +=  each.getCharge();
 
 	        }
 	        //add footer lines
